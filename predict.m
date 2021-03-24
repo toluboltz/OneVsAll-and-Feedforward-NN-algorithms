65c9e2609f983586b95cc1dd644bc1e9cd08815c
Tolulope Akinbulire <toluboltz@gmail.com>
2021-03-24 17:47:37 -0400

    Add MATLAB files for OneVsAll and feedforward neural network algorithms

---
+++ predict.m
@@ -0,0 +1,39 @@
+function p = predict(Theta1, Theta2, X)
+%PREDICT Predict the label of an input given a trained neural network
+%   p = PREDICT(Theta1, Theta2, X) outputs the predicted label of X given the
+%   trained weights of a neural network (Theta1, Theta2)
+
+% Useful values
+m = size(X, 1);
+num_labels = size(Theta2, 1);
+
+% You need to return the following variables correctly 
+p = zeros(size(X, 1), 1);
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Complete the following code to make predictions using
+%               your learned neural network. You should set p to a 
+%               vector containing labels between 1 to num_labels.
+%
+% Hint: The max function might come in useful. In particular, the max
+%       function can also return the index of the max element, for more
+%       information see 'help max'. If your examples are in rows, then, you
+%       can use max(A, [], 2) to obtain the max for each row.
+%
+
+X = [ones(m, 1), X];
+
+% compute the sigmoid (logistic) activation function for the second layer
+a2 = sigmoid(X * Theta1');
+
+% add the basic unit to the second layer
+a2 = [ones(size(a2, 1), 1), a2];
+% compute the sigmoid activation function for the third layer a3 == h
+h = sigmoid(a2 * Theta2');
+
+% predict the new outputs
+[_, p] = max(h, [], 2); 
+% =========================================================================
+
+
+end
